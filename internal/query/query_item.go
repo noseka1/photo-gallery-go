d264716247e1469bb76be79513f2c15ec47ad8b2
Ales Nosek <anosek@redhat.com>
2019-11-04 08:40:06 -0800

    Fix query_item.go formatting

--- internal/query/query_item.go
+++ internal/query/query_item.go
@@ -1,8 +1,8 @@
 package query
 
 type queryItem struct {
-	Id    int `json:"id" binding:"required"`
+	Id       int    `json:"id" binding:"required"`
 	Name     string `json:"name" binding:"required"`
 	Category string `json:"category" binding:"required"`
-	Likes int `json:"likes" binding:"required"`
+	Likes    int    `json:"likes" binding:"required"`
 }
